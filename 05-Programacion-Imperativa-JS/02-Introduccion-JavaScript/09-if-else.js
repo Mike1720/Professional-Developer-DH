fb67c2faac5f9c8f18c6893b74c816c0766b353d
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2024-12-01 19:22:05 -0600

    Estructura de control if-else

---
+++ 05-Programacion-Imperativa-JS/02-Introduccion-JavaScript/09-if-else.js
@@ -0,0 +1,22 @@
+// Estructura de control if-else
+/* 
+  La estructura de control if-else nos permite tomar decisiones en nuestro código, es decir,
+  ejecutar un bloque de código si se cumple una condición y otro bloque de código si no se 
+  cumple la condición.
+
+  Sintaxis:
+  if (condición) {
+    bloque de código si se cumple la condición
+  }
+  else {
+    bloque de código si no se cumple la condición
+  }
+*/
+
+let edad = 4;
+
+if (edad >= 18){
+  console.log('Eres mayor de edad');
+}else{
+  console.log('Eres menor de edad');
+}
