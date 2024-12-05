cf581ab7bab67df27495e96d3fbf22f0992d31d0
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2024-12-04 20:45:36 -0600

    Bucle: for

---
+++ 05-Programacion-Imperativa-JS/03-Intermedio-JavaScript/03-for.js
@@ -0,0 +1,14 @@
+// Bucle: for
+/* 
+  El bucle for es una estructura de control que se repite un bloque de código un número determinado de veces.
+*/
+
+//   inicialización; condición; incremento
+for (let i = 0; i <= 10; i += 3) {
+  console.log(i); // 0, 3, 6, 9
+}
+
+//   inicialización; condición; decremento
+for (let j = 10; j >= 0; j -= 2) {
+  console.log(j); // 10, 8, 6, 4, 2, 0 
+}
